a06e2d7359aaf8dd66cd40ec57f7230e3acaaf2e
Naveed Anjum <ndmiangee@yahoo.com>
2023-09-08 12:10:38 +0200

    hello world react

--- src/App.js
+++ src/App.js
@@ -2,5 +2,10 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <div>Say hello...</div>;
+  return (
+    <main>
+      <heading>Hello world </heading>
+      <p>this is react projeckt</p>
+    </main>
+  );
 }
